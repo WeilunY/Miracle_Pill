ad8117b5f13dce0f4c2df89583b233de67764f2a
WeilunY <yaoweilun1998@hotmail.com>
2017-05-18 21:26:49 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Miracle_Pill
+//
+//  Created by william yao on 5/18/17.
+//  Copyright © 2017 william yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // self.view.backgroundColor = UIColor.blue //set the background color
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
