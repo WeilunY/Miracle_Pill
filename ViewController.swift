3d33c31044f9917fa2b86476b9ca72da3b1ad39c
WeilunY <yaoweilun1998@hotmail.com>
2017-05-20 21:22:20 -0700

    added a state picker and btn

--- ViewController.swift
+++ ViewController.swift
@@ -8,12 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    //Initialize outlets
 
+    @IBOutlet weak var state_picker: UIPickerView!
+    
+    @IBOutlet weak var state_picker_btn: UIButton!
+    
+    let state = [ "AK - Alaska",
+                  "AL - Alabama",
+                  "AR - Arkansas",
+                  "AS - American Samoa",
+                  "AZ - Arizona",
+                  "CA - California",
+                  "CO - Colorado",
+                  "CT - Connecticut",
+                  "DC - District of Columbia",
+                  "DE - Delaware",
+                  "FL - Florida",
+                  "GA - Georgia",
+                  "GU - Guam",
+                  "HI - Hawaii",
+                  "IA - Iowa",
+                  "ID - Idaho",
+                  "IL - Illinois",
+                  "IN - Indiana",
+                  "KS - Kansas",
+                  "KY - Kentucky",
+                  "LA - Louisiana",
+                  "MA - Massachusetts",
+                  "MD - Maryland",
+                  "ME - Maine",
+                  "MI - Michigan",
+                  "MN - Minnesota",
+                  "MO - Missouri",
+                  "MS - Mississippi",
+                  "MT - Montana",
+                  "NC - North Carolina",
+                  "ND - North Dakota",
+                  "NE - Nebraska",
+                  "NH - New Hampshire",
+                  "NJ - New Jersey",
+                  "NM - New Mexico",
+                  "NV - Nevada",
+                  "NY - New York",
+                  "OH - Ohio",
+                  "OK - Oklahoma",
+                  "OR - Oregon",
+                  "PA - Pennsylvania",
+                  "PR - Puerto Rico",
+                  "RI - Rhode Island",
+                  "SC - South Carolina",
+                  "SD - South Dakota",
+                  "TN - Tennessee",
+                  "TX - Texas",
+                  "UT - Utah",
+                  "VA - Virginia",
+                  "VI - Virgin Islands",
+                  "VT - Vermont",
+                  "WA - Washington",
+                  "WI - Wisconsin",
+                  "WV - West Virginia", 
+                  "WY - Wyoming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // self.view.backgroundColor = UIColor.blue //set the background color
+        state_picker.dataSource = self
+        state_picker.delegate = self
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +85,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Set the action for pressing btn --> show the list
+    @IBAction func state_btn_press(_ sender: Any) {
+        state_picker.isHidden = false
+    }
+    
+    //Set the number of components from the state list we want
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //How many states
+        return 1
+    }
+    
+    //Set the number of states in the list
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return state.count
+    }
+    
+    //Set the return value
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return state[row]
+    }
+    
+    //Change the title of the btn once we select the name
+    //Then hide it
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        state_picker_btn.setTitle(state[row], for: UIControlState.normal)
+        state_picker.isHidden = true
+    }
 
 }
 
